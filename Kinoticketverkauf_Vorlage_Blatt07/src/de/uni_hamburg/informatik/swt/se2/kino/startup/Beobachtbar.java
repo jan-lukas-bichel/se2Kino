6a74bbbeeb98ce40e3f236857fd096d85a346897
Bungi77 <67256811+Bungi77@users.noreply.github.com>
2021-06-09 09:23:38 +0200

    Schnittstellen Kommentare

--- Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/startup/Beobachtbar.java
+++ Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/startup/Beobachtbar.java
@@ -3,20 +3,37 @@ package de.uni_hamburg.informatik.swt.se2.kino.startup;
 import java.util.HashSet;
 import java.util.Set;
 
-public abstract class Beobachtbar {
-	private Set<Beobachter> _beobachter;
+public abstract class Beobachtbar
+{
 
-	public Beobachtbar() {
-		_beobachter = new HashSet<Beobachter>();
-	}
+    // Dieses Set Speichert die Beobachter die sich Registriert haben 
+    private Set<Beobachter> _beobachter;
 
-	public void fuegeBeobachterHinzu(Beobachter beobachter) {
-		_beobachter.add(beobachter);
-	}
+    public Beobachtbar()
+    {
+        _beobachter = new HashSet<Beobachter>();
+    }
 
-	public void informiereUeberAenderung() {
-		for (Beobachter beobachter : _beobachter) {
-			beobachter.reagiereAufAenderung(this);
-		}
-	}
+    /**
+     * Fügt ein Beobachter dem Set hinzu
+     * 
+     * @param beobachter der dem Set hinzugefügt werden soll
+     * @require beobachter != null
+     */
+    public void fuegeBeobachterHinzu(Beobachter beobachter)
+    {
+        assert beobachter != null : "Vorbedingung verletzt: beobachter ist null";
+        _beobachter.add(beobachter);
+    }
+
+    /**
+     * Informiert jeden Beobachter über Änderungen  
+     */
+    public void informiereUeberAenderung()
+    {
+        for (Beobachter beobachter : _beobachter)
+        {
+            beobachter.reagiereAufAenderung(this);
+        }
+    }
 }
